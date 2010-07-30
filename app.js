d1ff3da19ad22984a1d36e10dd12a510cc64f171
Timo Derstappen <teemow@ross.teemow.com>
2010-07-30 12:36:12 +0000

    initial express scaffold

---
+++ app.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    connect = require('connect');
+
+// Create and export Express app
+
+var app = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.use(connect.bodyDecoder());
+    app.use(connect.methodOverride());
+    app.use(connect.compiler({ src: __dirname + '/public', enable: ['sass'] }));
+    app.use(connect.staticProvider(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+    app.set('reload views', 1000);
+    app.use(connect.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+   app.use(connect.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+    res.render('index.jade', {
+        locals: {
+            title: 'Express'
+        }
+    });
+});
+
+app.listen(3333);
