04ddd888b87e3a0a265d5c7887faae88a5295720
Timo Derstappen <teemow@ross.teemow.com>
2010-07-30 12:58:29 +0000

    added next meetup to index

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.configure('production', function(){
 app.get('/', function(req, res){
     res.render('index.jade', {
         locals: {
-            title: 'Express'
+            title: 'Cologne.js'
         }
     });
 });
